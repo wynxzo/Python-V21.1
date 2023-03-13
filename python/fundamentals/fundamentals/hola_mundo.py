c9c6d612f46a9d322a28be1ab0f7a26163764b93
wynxzo <isidora.poblete.sepulveda@alumnos.sip.cl>
2023-03-13 14:42:55 -0300

    primer commit

---
+++ python/fundamentals/fundamentals/hola_mundo.py
@@ -0,0 +1,25 @@
+import random
+
+print('HOLLAAAAAAA!')
+print('BIENVENIDO A PYTHONNNNNNNN')
+
+print('This is a loop printing 5 times')
+for x in range(1, 6):
+    print(f'x is: {x}')
+
+weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+day = random.choice(weekdays)
+
+print(f'Today is: {day}')
+
+if day == 'Monday':
+    print('It will be a long week!')
+elif(day == 'Friday'):
+    print('Woohoo, time for the weekend!')
+else:
+    print('Not quite there yet.')
+    x = "Hola, Python"
+print(x)
+y = 42
+print(y)
+
